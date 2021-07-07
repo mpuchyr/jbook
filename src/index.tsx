a5c1cabfa97c04b2c2895dae2a4de434352bd5aa
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-07 16:31:51 -0500

    Create onClick function in src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const onClick = () => {
+        console.log(input)
+    }
+
     return (
         <div>
-            <textarea></textarea>
+            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
             <div>
-                <button>Submit</button>
+                <button onClick={onClick}>Submit</button>
             </div>
-            <pre></pre>
+            <pre>{code}</pre>
         </div>
     )
 }
