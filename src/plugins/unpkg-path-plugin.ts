9502fd537c38d0270c856b0f6b2daf8c55553d07
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-09 13:56:12 -0500

    Update unpkgPathPlugin to take an argument in src/plugins/unpkg-path-plugin.ts

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -8,7 +8,7 @@ const fileCache = localForage.createInstance({
 
 
  
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -38,10 +38,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, { useState } from 'react';
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         }
 
