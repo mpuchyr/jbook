f233d7a97258ad8fad8f6518287dd89932382ead
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-15 12:15:55 -0500

    Modify components/resizable.tsx to resize with window height and width

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 interface ResizableProps {
@@ -6,13 +7,35 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
     let resizeableProps: ResizableBoxProps
+
+    useEffect(() => {
+        let timer: any
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer)
+            }
+            
+            timer = setTimeout (() => {
+                setInnerHeight(window.innerHeight)
+                setInnerWidth(window.innerWidth)
+            }, 100)
+            
+        }
+        window.addEventListener('resize', listener)
+
+        return () => {
+            window.removeEventListener('resize', listener)
+        }
+    }, [])
 
     if (direction === 'horizontal') {
         resizeableProps = {
             className: 'resize-horizontal',
-            minConstraints: [window.innerWidth * 0.2, Infinity],
-            maxConstraints: [window.innerWidth * 0.75, Infinity],
+            minConstraints: [innerWidth * 0.2, Infinity],
+            maxConstraints: [innerWidth * 0.75, Infinity],
             height: Infinity,
             width: window.innerWidth * 0.75,
             resizeHandles: ['e']
@@ -20,7 +43,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     } else {
         resizeableProps = {
             minConstraints: [Infinity, 24],
-            maxConstraints: [Infinity, window.innerHeight * 0.9],
+            maxConstraints: [Infinity, innerHeight * 0.9],
             height: 300,
             width: Infinity,
             resizeHandles: ['s']
