85e594bedc23df42a5aacd94e501a183627f355e
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-21 16:15:36 -0500

    Update ActionType.INSERT_CELL_BEFORE case in state/reducers/cellsReducer.ts

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -21,7 +21,7 @@ export interface DeleteCellAction {
 export interface InsertCellBeforeAction {
     type: ActionType.INSERT_CELL_BEFORE;
     payload: {
-        id: string;
+        id: string | null;
         type: CellTypes;
     }
 }
