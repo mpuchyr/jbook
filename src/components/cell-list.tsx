9934f265bddddaa60c39512179e43c284a9374d6
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-22 14:30:02 -0500

    Add initialState as second argument in state/reducers/cellsReducer.ts to fix an error

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,12 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
 
 const CellList: React.FC = () => {
+    useTypedSelector(({ cells: { order, data }}) => {
+        return order.map((id) => {
+            return data[id]
+        })
+    })
+    
     return <div>Cell List</div>
 }
 
