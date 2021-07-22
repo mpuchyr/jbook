9eb1a434369de761fedae9adf4c78a0c61b49abe
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-22 14:11:50 -0500

    Import CellList into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
-import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 
 const App = () => {
@@ -11,7 +10,7 @@ const App = () => {
     return (
         <Provider store={store}>
             <div>
-                <TextEditor />
+                <CellList />
             </div>
         </Provider>
     )
