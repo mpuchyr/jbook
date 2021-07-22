926da78f33ea20fbbbff67aad620df2b917ab17e
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-22 14:35:55 -0500

    Create cells and renderedCells variables in components/cell-list.tsx

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,13 +1,16 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
-    useTypedSelector(({ cells: { order, data }}) => {
+    const cells = useTypedSelector(({ cells: { order, data }}) => {
         return order.map((id) => {
             return data[id]
         })
     })
+
+    const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell}></CellListItem>)
     
-    return <div>Cell List</div>
+    return <div>{renderedCells}</div>
 }
 
 export default CellList
