08bf4fa3abc637072ada0e72ac3207b36f356c1d
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-22 14:47:20 -0500

    Create hooks/use-actions.ts

--- src/state/store.ts
+++ src/state/store.ts
@@ -21,3 +21,20 @@ store.dispatch({
     }
 })
 
+store.dispatch({
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type: 'code'
+    }
+})
+
+store.dispatch({
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type: 'text'
+    }
+})
+
+
