55a85fdf00b00b4606e8162a1abc58604f84dd78
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-07-27 15:07:18 -0500

    Create export const createBundle in state/action-creators/index.ts

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,6 +1,17 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
-import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellAfterAction, Direction } from '../actions';
+import { 
+    Action,
+    UpdateCellAction, 
+    DeleteCellAction, 
+    MoveCellAction, 
+    InsertCellAfterAction, 
+    BundleStartAction,
+    BundleCompleteAction,
+    Direction
+} from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction  => {
     return {
@@ -36,5 +47,26 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
             id,
             type: cellType
         }
+    }
+}
+
+export const createBundle = (cellId: string, input: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BUNDLE_START,
+            payload: {
+                cellId
+            }
+        })
+
+        const result = await bundle(input)
+
+        dispatch({
+            type: ActionType.BUNDLE_COMPLETE,
+            payload: {
+                cellId,
+                bundle: result
+            }
+        })
     }
 }
